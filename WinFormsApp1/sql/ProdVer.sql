2a0b126d93a6bc83ea3c7d4f575b0cbc6dc28b76
rjpd@netzero.com <rogerjd>
2019-06-09 13:57:41 -0400

    sql, temp table, and prod ver

--- WinFormsApp1/sql/ProdVer.sql
+++ WinFormsApp1/sql/ProdVer.sql
@@ -4,14 +4,33 @@ from RawBCs
 select *
 from Inventory
 
+drop table #tmp
+select * into #tmp from (select * from Inventory) as x
+select * from #tmp
 
-select rb.RawBC
+select x.Id, x.RawBC, x.NDC, x.DrugName into #temp from (
+select * --distinct(rb.RawBC)
 from RawBCs rb
 left join Inventory i on rb.RawBC = i.NDC
 left join Inventory i2 on rb.NDC = i2.NDC
-where i.NDC is null
+where i.NDC is null and i2.NDC is not null ) as x
+
+select * --distinct(rb.RawBC)
+from RawBCs rb
+left join Inventory i on rb.RawBC = i.NDC
+left join Inventory i2 on rb.NDC = i2.NDC
+where i.NDC is null and i2.NDC is not null
+
+
+
+
+select rb.RawBC -- * --distinct(rb.RawBC)
+from RawBCs rb
+left join Inventory i on rb.RawBC = i.NDC
+left join Inventory i2 on rb.NDC = i2.NDC
+where i.NDC is null --and i2.NDC is not null
 group by rb.RawBC 
-having COUNT(distinct rb.ndc) = 1
+having (COUNT(distinct rb.ndc) = 1) --and i2.NDC is not null
 
 /*
 select rb.RawBC
